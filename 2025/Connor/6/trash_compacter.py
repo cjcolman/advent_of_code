dab1e0677d50c5597994a6277b800be17565fc1d
cjcolman <54371154+cjcolman@users.noreply.github.com>
2025-12-08 11:27:03 +0000

    2025 Day 6

--- 2025/Connor/6/trash_compacter.py
+++ 2025/Connor/6/trash_compacter.py
@@ -17,17 +17,16 @@ def perform_operation(data):
     return local_vars['output']
 
 # %%
-def sum_all_operations(file_path):
-    list_of_operations = parse_input(file_path)
+def sum_all_operations(list_of_operations):
     total = 0
     for operation in list_of_operations:
         total += perform_operation(operation)
     return total
 
 # %%
-print("Example: ", sum_all_operations("example.txt"), " Expected: 4277556")
+print("Example: ", sum_all_operations(parse_input("example.txt")), " Expected: 4277556")
 
-print("Part 1: ", sum_all_operations("input.txt"))
+print("Part 1: ", sum_all_operations(parse_input("input.txt")))
 
 # %%
 def parse_input_p2(filepath):
@@ -39,7 +38,7 @@ def parse_input_p2(filepath):
         for _ in range(extra):
             data[i].append('')
 
-    output = [[] for i in range(len(data))]
+    output = [[] for i in range(len(data[0]))]
     n = 0
     while data[0]:
         try:
@@ -50,8 +49,29 @@ def parse_input_p2(filepath):
             break
     return output
 
+# %%
+def parse_operations(ls):
+    output = []
+    while ls:
+        current_op = []
+        while ls:
+            if ls[0] == [' ' for i in range(len(ls[0]))]:
+                break
+            current_op.append(ls.pop(0))
+        current_parsed_op = []
+        for i in range(len(current_op)-1):
+            current_parsed_op.append((''.join(current_op[i])).strip())
+        current_parsed_op.append((''.join(current_op[-1][:-1])).strip())
+        current_parsed_op.append(current_op[-1][-1])
+        output.append(current_parsed_op)
+        if ls:
+            _ = ls.pop(0)  # remove separator line
+    return output
+
 
 # %%
-print(parse_input_p2("example.txt"))
+print("Part 2 Example: ", sum_all_operations(parse_operations(parse_input_p2("example.txt"))), " Expected: 3263827")
+print("Part 2: ", sum_all_operations(parse_operations(parse_input_p2("input.txt"))))
+
 
 # %%
